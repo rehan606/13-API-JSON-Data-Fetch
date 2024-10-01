66ae30894553c0780d72912bf9fa1ab9d9a0596c
rehan606 <rehanlemu@gmail.com>
2024-10-01 19:46:48 +0600

    API JSON DATA FETCHING

---
+++ json.js
@@ -0,0 +1,48 @@
+
+
+
+// Display Load Post 
+
+function loadPost(){
+      fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => displayPost(data) )
+}
+
+
+function displayPost(data){
+      const allPost = document.getElementById('post-data')
+      for (const post of data){
+            const div = document.createElement('div')
+           
+            div.innerHTML = `
+                  <h4 class="">id: ${post.id} </h4>
+                  <p class="text-blue-800">Title: ${post.title} </p>
+                  <p class="">Body: ${post.body} </p>
+            `
+            allPost.appendChild(div)
+      }
+}
+
+loadPost()
+
+
+
+// Display JSON user data when Button click
+function userData(data){
+      const apiUr = 'https://jsonplaceholder.typicode.com/users'
+      fetch(apiUr)
+      .then(response => response.json())
+      .then(data => displayUsers(data))
+
+}
+
+function displayUsers(data){
+      const usrList = document.getElementById('user-list')
+      for(const user of data){
+            const li = document.createElement('li')
+            li.innerText = user.name
+            
+            usrList.appendChild(li)
+      }
+}
